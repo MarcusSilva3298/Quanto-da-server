0eb871cd1166e82a7df53c09c3022a4a0c6169ea
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-07-18 11:38:53 -0300

    feat(graphql): added base nest-graphql libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
