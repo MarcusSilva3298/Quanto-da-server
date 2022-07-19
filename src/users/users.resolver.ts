478bfd567084c409df830fddbf302ffe18b7af56
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-07-19 19:43:06 -0300

    refactor(removed unused imports): removed unused imports

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
 import { User } from './entities/user.entity'
 import { CreateUserInput } from './dto/create-user.input'
 import { UpdateUserInput } from './dto/update-user.input'
