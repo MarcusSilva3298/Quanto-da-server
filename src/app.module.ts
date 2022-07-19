4901706efc30de7ba97c9764dd57fcc7ed4f126b
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-07-19 19:42:26 -0300

    feat(shared module): added a shared module + bcrypt service inside of it

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 import { UsersModule } from './users/users.module'
+import { SharedModule } from './shared/shared.module'
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { UsersModule } from './users/users.module'
       autoSchemaFile: join(process.cwd(), 'src/schema.gql')
     }),
     PrismaModule,
-    UsersModule
+    UsersModule,
+    SharedModule
   ],
   controllers: [AppController],
   providers: [AppService]
