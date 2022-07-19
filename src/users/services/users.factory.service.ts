9ca609e22b9d10318812c5634fd597b89c41c949
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-07-19 19:46:01 -0300

    feat(new methods): create, update & remove methods

--- src/users/services/users.factory.service.ts
+++ src/users/services/users.factory.service.ts
@@ -1,18 +1,60 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+
+import { User } from '../entities/user.entity'
 import { CreateUserInput } from '../dto/create-user.input'
 import { UpdateUserInput } from '../dto/update-user.input'
 
+import { UsersRepository } from '../users.repository'
+import { UsersQueryService } from './users.query.service'
+import { BcryptService } from '../../shared/bcrypt.service'
+
 @Injectable()
 export class UsersFactoryService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user'
+  constructor(
+    private usersQuery: UsersQueryService,
+    private usersRepository: UsersRepository,
+    private bcryptService: BcryptService
+  ) {}
+
+  async create({
+    cpf,
+    email,
+    password,
+    first_name,
+    last_name,
+    phone
+  }: CreateUserInput): Promise<User> {
+    const cpfInUse = await this.usersRepository.findByCPF(cpf)
+
+    if (cpfInUse) throw new BadRequestException('CPF already in use')
+
+    const emailInUse = await this.usersRepository.findByEmail(email)
+
+    if (emailInUse) throw new BadRequestException('Email already in use')
+
+    const hashedPassword = await this.bcryptService.hashPassword(password)
+
+    return await this.usersRepository.create({
+      cpf,
+      email,
+      password: hashedPassword,
+      first_name,
+      last_name,
+      phone
+    })
   }
 
-  update(id: string, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`
+  async update(id: string, updateUserInput: UpdateUserInput) {
+    await this.usersQuery.findById(id)
+
+    return await this.usersRepository.update(id, updateUserInput)
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} user`
+  async remove(id: string) {
+    await this.usersQuery.findById(id)
+
+    await this.usersRepository.remove(id)
+
+    return `User with id:${id} deleted`
   }
 }
