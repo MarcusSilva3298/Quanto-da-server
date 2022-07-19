c1f390a48357aa9048333e84613f5921710cbac7
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-07-19 19:43:50 -0300

    feat(users imports + providers): imported SharedModule + UsersRepository as provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from '../prisma/prisma.module'
+import { SharedModule } from '../shared/shared.module'
 import { UsersFactoryService } from './services/users.factory.service'
 import { UsersQueryService } from './services/users.query.service'
 import { UsersService } from './services/users.service'
+import { UsersRepository } from './users.repository'
 import { UsersResolver } from './users.resolver'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SharedModule],
   providers: [
     UsersResolver,
     UsersService,
     UsersQueryService,
-    UsersFactoryService
+    UsersFactoryService,
+    UsersRepository
   ]
 })
 export class UsersModule {}
