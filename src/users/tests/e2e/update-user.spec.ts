f598f953189ea1d11918d2280cbb9d7399247f73
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-08-16 12:05:19 -0300

    test(update user): modified tests to be more realistic + added missing test

--- src/users/tests/e2e/update-user.spec.ts
+++ src/users/tests/e2e/update-user.spec.ts
@@ -3,13 +3,13 @@ import * as request from 'supertest'
 import { Test } from '@nestjs/testing'
 import { AppModule } from '../../../app.module'
 import { PrismaService } from '../../../prisma/prisma.service'
-import { UsersRepository } from '../../users.repository'
 import { UserMock } from '../mocks/users.entity'
+import { User } from '../../entities/user.entity'
 
 describe('Update User', () => {
   let app: NestApplication
   let prisma: PrismaService
-  let repository: UsersRepository
+  let user: User
   const mutation = () => `
     mutation updateUser(
       $updateUserInput: UpdateUserInput!
@@ -27,9 +27,12 @@ describe('Update User', () => {
 
     app = moduleFixture.createNestApplication()
     prisma = moduleFixture.get<PrismaService>(PrismaService)
-    repository = moduleFixture.get<UsersRepository>(UsersRepository)
 
     await prisma.clear()
+
+    user = await prisma.users.create({
+      data: new UserMock()
+    })
 
     await app.init()
   })
@@ -39,17 +42,11 @@ describe('Update User', () => {
   })
 
   it('Should be able to update user - only first name', async () => {
-    const user = new UserMock()
-    jest.spyOn(repository, 'findById').mockImplementationOnce(async () => user)
-
-    const updatedUser = user.update({ first_name: true })
-    jest
-      .spyOn(repository, 'update')
-      .mockImplementationOnce(async () => updatedUser)
+    const updateUserData = new UserMock().update({ first_name: true })
 
     const updateUserInput = {
       id: user.id,
-      first_name: updatedUser.first_name
+      first_name: updateUserData.first_name
     }
 
     const result = await request(app.getHttpServer()).post('/graphql').send({
@@ -62,23 +59,17 @@ describe('Update User', () => {
     expect(updateUser).toEqual(
       expect.objectContaining({
         id: user.id,
-        first_name: updatedUser.first_name
+        first_name: updateUserData.first_name
       })
     )
   })
 
   it('Should be able to update user - only last name', async () => {
-    const user = new UserMock()
-    jest.spyOn(repository, 'findById').mockImplementationOnce(async () => user)
-
-    const updatedUser = user.update({ last_name: true })
-    jest
-      .spyOn(repository, 'update')
-      .mockImplementationOnce(async () => updatedUser)
+    const updateUserData = new UserMock().update({ last_name: true })
 
     const updateUserInput = {
       id: user.id,
-      last_name: updatedUser.last_name
+      last_name: updateUserData.last_name
     }
 
     const result = await request(app.getHttpServer()).post('/graphql').send({
@@ -91,23 +82,17 @@ describe('Update User', () => {
     expect(updateUser).toEqual(
       expect.objectContaining({
         id: user.id,
-        last_name: updatedUser.last_name
+        last_name: updateUserData.last_name
       })
     )
   })
 
   it('Should be able to update user - only phone', async () => {
-    const user = new UserMock()
-    jest.spyOn(repository, 'findById').mockImplementationOnce(async () => user)
-
-    const updatedUser = user.update({ phone: true })
-    jest
-      .spyOn(repository, 'update')
-      .mockImplementationOnce(async () => updatedUser)
+    const updateUserData = new UserMock().update({ phone: true })
 
     const updateUserInput = {
       id: user.id,
-      phone: updatedUser.phone
+      phone: updateUserData.phone
     }
 
     const result = await request(app.getHttpServer()).post('/graphql').send({
@@ -120,29 +105,23 @@ describe('Update User', () => {
     expect(updateUser).toEqual(
       expect.objectContaining({
         id: user.id,
-        phone: updatedUser.phone
+        phone: updateUserData.phone
       })
     )
   })
 
   it('Should be able to update user - full', async () => {
-    const user = new UserMock()
-    jest.spyOn(repository, 'findById').mockImplementationOnce(async () => user)
-
-    const updatedUser = user.update({
-      first_name: true,
+    const updateUserData = new UserMock().update({
+      phone: true,
       last_name: true,
-      phone: true
+      first_name: true
     })
-    jest
-      .spyOn(repository, 'update')
-      .mockImplementationOnce(async () => updatedUser)
 
     const updateUserInput = {
       id: user.id,
-      first_name: updatedUser.first_name,
-      last_name: updatedUser.last_name,
-      phone: updatedUser.phone
+      first_name: updateUserData.first_name,
+      last_name: updateUserData.last_name,
+      phone: updateUserData.phone
     }
 
     const result = await request(app.getHttpServer()).post('/graphql').send({
@@ -155,9 +134,29 @@ describe('Update User', () => {
     expect(updateUser).toEqual(
       expect.objectContaining({
         id: user.id,
-        first_name: updatedUser.first_name,
-        last_name: updatedUser.last_name,
-        phone: updatedUser.phone
+        first_name: updateUserData.first_name,
+        last_name: updateUserData.last_name,
+        phone: updateUserData.phone
+      })
+    )
+  })
+
+  it('Should not be able to update user with wrong id', async () => {
+    const updateUserInput = {
+      id: '123'
+    }
+
+    const result = await request(app.getHttpServer()).post('/graphql').send({
+      query: mutation(),
+      variables: { updateUserInput }
+    })
+
+    const { errors } = result.body
+
+    expect(errors).toHaveLength(1)
+    expect(errors[0]).toEqual(
+      expect.objectContaining({
+        message: expect.stringMatching('User not found with id:123')
       })
     )
   })
